accc54b5b96bc467fc2b692c93b6c6b13a43b73f
buched <32975584+buched@users.noreply.github.com>
2025-05-13 12:42:10 +0200

    Update bee_sensor_sam_esp32.ino

--- bee_sensor_sam_esp32.ino
+++ bee_sensor_sam_esp32.ino
@@ -8,20 +8,22 @@ HardwareSerial MySerial(1);
 #define HX_SCK 18
 #define HX_VCC_CTRL 23
 
-// SigFox UART
+// SigFox UART (UART2)
 #define SIGFOX_TX 16
 #define SIGFOX_RX 17
 #define SIGFOX_BAUD 9600
 
 // Capteur anti-vol
 #define GPIO_WAKEUP 33
+char idStr[9] = ""; // 8 chars + null
+int i = 0;
 
 HX711 scale;
 
 void setup() {
   Serial.begin(115200);
-  MySerial.begin(9600, SERIAL_8N1, 16, 17); // serial port to send RTCM to F9P
-  delay(100);
+  MySerial.begin(9600, SERIAL_8N1, 17, 16); // port série du module sigfox
+    delay(100);
   // Alimentation du HX711 via GPIO
   pinMode(HX_VCC_CTRL, OUTPUT);
   digitalWrite(HX_VCC_CTRL, HIGH);
@@ -29,38 +31,23 @@ void setup() {
 
   // Initialisation HX711
   scale.begin(HX_DT, HX_SCK);
-  scale.set_scale(41.1);  // Ajuste selon ton calibrage
+  scale.set_scale(48.182);  // Ajuste selon ton calibrage
+  //scale.tare();
+  // Faire la tare uniquement au tout premier démarrage (alimentation)
+if (esp_reset_reason() == ESP_RST_POWERON) {
   scale.tare();
+}
   delay(100);
-  // Lire l'ID SigFox depuis le module
-  char idStr[9] = ""; // 8 chars + null
-  int i = 0;
 
   MySerial.println("AT$I=10");
   delay(300);
-
-  unsigned long timeout = millis() + 1000;
-  while (millis() < timeout && i < 8)
-    {
-      if (MySerial.available())
-        {
-          char c = MySerial.read();
-          if (isxdigit(c))
-            {
-              idStr[i++] = c;
-            }
-        }
-    }
-  idStr[8] = '\0'; // sécurité
-
-  // Convertir chaîne hex en uint32_t
-  uint32_t id = strtoul(idStr, NULL, 16);
-
+  
+  uint32_t id = 0x02DF9FA2; // à remplacer par l'id de votre module sigfox sur 8 caracteres donc avec un 0 au debut
   // Lecture poids
   float poids_g = scale.get_units(10);
   if (poids_g < 0) poids_g = 0;
   uint32_t poids10 = (uint32_t)(poids_g * 10);
-  
+  Serial.println(poids10);
   // Créer payload (little-endian)
   byte payload[8];
   payload[0] = id & 0xFF;
@@ -71,13 +58,13 @@ void setup() {
   payload[5] = (poids10 >> 8) & 0xFF;
   payload[6] = (poids10 >> 16) & 0xFF;
   payload[7] = (poids10 >> 24) & 0xFF;
-
+  
   // Construire payload hex pour AT$SF
   char hexStr[17];
   sprintf(hexStr, "%02X%02X%02X%02X%02X%02X%02X%02X",
-  payload[0], payload[1], payload[2], payload[3],
-  payload[4], payload[5], payload[6], payload[7]);
-
+    payload[0], payload[1], payload[2], payload[3],
+    payload[4], payload[5], payload[6], payload[7]);
+  
   MySerial.print("AT$SF=");
   MySerial.println(hexStr);
   delay(1500);
@@ -86,13 +73,14 @@ void setup() {
   digitalWrite(HX_VCC_CTRL, LOW);
 
   // Configuration du réveil par interruption GPIO33 (anti-vol)
-  pinMode(GPIO_WAKEUP, INPUT_PULLUP); // bouton/contact = niveau bas = alerte
-  esp_sleep_enable_ext0_wakeup((gpio_num_t)GPIO_WAKEUP, 0); // 0 = niveau bas déclencheur
+  //pinMode(GPIO_WAKEUP, INPUT_PULLUP); // bouton/contact = niveau bas = alerte
+  //esp_sleep_enable_ext0_wakeup((gpio_num_t)GPIO_WAKEUP, 0); // 0 = niveau bas déclencheur
 
   // Réveil automatique
   //esp_sleep_enable_timer_wakeup(3ULL * 60ULL * 60ULL * 1000000ULL);  // 3 heures
   esp_sleep_enable_timer_wakeup(1800ULL * 1000000ULL);  // 30 minutes
   //esp_sleep_enable_timer_wakeup(600ULL * 1000000ULL);  // 10 minutes
+  
   esp_deep_sleep_start();
 }
 
