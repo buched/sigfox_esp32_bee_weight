f811b76c4ba137abb863cb0e64d1ecbde92c070f
buched <32975584+buched@users.noreply.github.com>
2025-07-08 20:12:23 +0200

    Update bee_sensor_sam_esp32.ino

--- bee_sensor_sam_esp32.ino
+++ bee_sensor_sam_esp32.ino
@@ -10,11 +10,13 @@ HardwareSerial MySerial(1);
 
 // Capteur anti-vol
 //#define GPIO_WAKEUP 1
+char idStr[9] = ""; // 8 chars + null
+int i = 0;
 
 HX711 scale;
 
 // Variable conservée pendant le deep sleep uniquement
-RTC_DATA_ATTR bool dejaInitialise = false;
+//RTC_DATA_ATTR bool dejaInitialise = false;
 
 bool envoyerSigfoxAvecRetries(String hexPayload) {
   const int MAX_RETRIES = 3;
@@ -55,63 +57,64 @@ bool envoyerSigfoxAvecRetries(String hexPayload) {
 
 void setup() {
   Serial.begin(115200);
-  delay(1000); // stabilité
-  MySerial.begin(9600, SERIAL_8N1, 21, 20);
-  delay(1000); // attendre que le module SigFox soit prêt
-
-  // Alimentation HX711
+  MySerial.begin(9600, SERIAL_8N1, 21, 20); // port carte sigfox
+    delay(100);
+    
+   Serial.println("setup");
+  // Alimentation du HX711 via GPIO
   pinMode(HX_VCC_CTRL, OUTPUT);
   digitalWrite(HX_VCC_CTRL, HIGH);
-  delay(1000);
-
-  // Init HX711
+  delay(1000);  // Laisser le capteur démarrer
+  // Initialisation HX711
   scale.begin(HX_DT, HX_SCK);
-  scale.set_scale(48.182);
-    // Faire la tare uniquement au tout premier démarrage (alimentation), Nb, normalement décommenté mais certains esp chinois ne supportent pas cette fonction
-  if (!dejaInitialise) 
+  // facteur à calibrer grace à l'ino joint
+  scale.set_scale(48.182);  // Ajuste selon ton calibrage
+  // Faire la tare uniquement au tout premier démarrage (alimentation)
+  if (esp_reset_reason() == ESP_RST_POWERON) {
+    Serial.println("tare");
+    scale.tare(); // tare uniquement au premier démarrage
+  }
+  else
     {
-      scale.tare();
+      Serial.println("tare deja effectué");
     }
-  delay(100);
-
+  delay(1500);
   // Lecture poids
   float poids_g = scale.get_units(10);
   if (poids_g < 0) poids_g = 0;
   uint32_t poids10 = (uint32_t)(poids_g * 10);
-  Serial.print("Poids (x10g) : ");
   Serial.println(poids10);
-
-  // Payload (4 octets, little-endian)
+  // Créer payload (little-endian)
   byte payload[4];
   payload[0] = poids10 & 0xFF;
   payload[1] = (poids10 >> 8) & 0xFF;
   payload[2] = (poids10 >> 16) & 0xFF;
   payload[3] = (poids10 >> 24) & 0xFF;
-
+  
   // Hex string
   char hexStr[9];
   sprintf(hexStr, "%02X%02X%02X%02X", payload[0], payload[1], payload[2], payload[3]);
   String hexPayload = String(hexStr);
   Serial.print("Payload Hex : ");
   Serial.println(hexPayload);
-
-  // Envoi avec 3 tentatives
+  
+  // Envoi avec tentatives
   bool success = envoyerSigfoxAvecRetries(hexPayload);
+  delay(1500);
 
-  // Extinction HX711
+  // Coupure du capteur pour l'économie d'énergie
   digitalWrite(HX_VCC_CTRL, LOW);
 
   // Configuration du réveil par interruption GPIO33 (anti-vol)
   //pinMode(GPIO_WAKEUP, INPUT_PULLUP); // bouton/contact = niveau bas = alerte
   //esp_sleep_enable_ext0_wakeup((gpio_num_t)GPIO_WAKEUP, 0); // 0 = niveau bas déclencheur
-  
-  // Wakeup, decommenter la ligen désirée et commenter les 2 autres
-  //esp_sleep_enable_timer_wakeup(600ULL * 1000000ULL);  // 10 minutes
-  esp_sleep_enable_timer_wakeup(1800ULL * 1000000ULL);  // 30 minutes
+
+  // Réveil automatique
   //esp_sleep_enable_timer_wakeup(3ULL * 60ULL * 60ULL * 1000000ULL);  // 3 heures
-  Serial.println("Mise en veille profonde...");
-  delay(100);
+  esp_sleep_enable_timer_wakeup(1700ULL * 1000000ULL);  // 30 minutes
+  //esp_sleep_enable_timer_wakeup(600ULL * 1000000ULL);  // 10 minutes
+  
   esp_deep_sleep_start();
 }
 
-void loop() {}
+void loop() { }
